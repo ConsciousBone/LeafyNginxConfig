d06149965473755ec29a0ae59ce477cf2ab46d7d
Evan <684295595+ConsciousBone@users.noreply.github.com>
2025-10-23 20:08:55 +0100

    Stupid colon

--- startproxy.sh
+++ startproxy.sh
@@ -3,5 +3,5 @@
 cd "$(dirname "$0")" # for sanity, move to the proper dir
 
 # grab the key, and launch nginx w/ the right config
-export OPENROUTER_API_KEY=$(cat apikey.txt | tr -d '\n')
+export OPENROUTER_API_KEY="$(tr -d '\r\n' < apikey.txt)"
 sudo nginx -p "$PWD" -c "$PWD/nginx.conf"
