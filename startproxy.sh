181c3e8200e3adb250e673c31692f8770d779ae4
Evan <684295595+ConsciousBone@users.noreply.github.com>
2025-10-23 19:53:23 +0100

    Nginx is hardddd

--- startproxy.sh
+++ startproxy.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")" # for sanity, move to the proper dir
 
 # grab the key, and launch nginx w/ the right config
 export OPENROUTER_API_KEY=$(cat apikey.txt | tr -d '\n')
-sudo nginx -p "$PWD" -c "$PWD/openrouter-proxy.conf"
+sudo nginx -p "$PWD" -c "$PWD/nginx.conf"
